dea88fa755fa3cbacdf5de624e5e0b4a15358659
stuti12 <stutibhavsar1@gmail.com>
2022-02-23 11:53:38 +0530

    Basic Auto LAyout Exercise with Comment Resolved

---
+++ myapp/Basic AutoLayout/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  myapp
+//
+//  Created by Stuti Bhavsar  on 15/04/22.
+//
+
+import Foundation
+struct Constants {
+    static let signupViewController = "goToSignUp"
+}
